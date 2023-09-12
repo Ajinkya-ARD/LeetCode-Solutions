fd50d730e7daf7355fca2d9877c098467a244906
Ajinkya Ravindra Deokate <129925084+Ajinkya-ARD@users.noreply.github.com>
2023-09-12 21:51:44 +0530

    LeetCode Solution #10
    894. All Possible Full Binary Trees
    Medium
    Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
    
    Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
    
    A full binary tree is a binary tree where each node has exactly 0 or 2 children.

---
+++ All Possible Full Binary Trees.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getTrees(self, n: int) -> List[Optional[TreeNode]]:
+        trees = []
+        if n == 1:
+            return [TreeNode()]
+        if n in self.cache:
+            return self.cache[n]
+        for i in range(1, n, 2):
+            lefts = self.getTrees(i)
+            rights = self.getTrees(n - i - 1)
+            for left in lefts:
+                for right in rights:
+                    root = TreeNode()
+                    root.left = left
+                    root.right = right
+                    trees.append(root)
+        self.cache[n] = trees
+        return trees
+
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        self.cache = {}
+        return self.getTrees(n)
