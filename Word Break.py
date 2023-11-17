326b5e8b07da79fb874936def4e951106facba83
Ajinkya Ravindra Deokate <129925084+Ajinkya-ARD@users.noreply.github.com>
2023-11-17 18:12:10 +0530

    LeetCode Solution #27
    139. Word Break
    Medium
    
    Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
    
    Note that the same word in the dictionary may be reused multiple times in the segmentation.

---
+++ Word Break.py
@@ -0,0 +1,12 @@
+class Solution:
+  def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+    n = len(s)
+    wordSet = set(wordDict)
+    dp = [True] + [False] * n 
+    for i in range(1, n + 1):
+      for j in range(i):
+
+        if dp[j] and s[j:i] in wordSet:
+          dp[i] = True
+          break
+    return dp[n]
