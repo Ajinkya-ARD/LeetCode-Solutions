dd08a735b6d83f012cbc375d710f535ed3de4e10
Ajinkya Ravindra Deokate <129925084+Ajinkya-ARD@users.noreply.github.com>
2023-11-25 17:01:36 +0530

    LeetCode Solution #46
    168. Excel Sheet Column Title
    Easy
    
    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
    
    For example:
    
    A -> 1
    B -> 2
    C -> 3
    ...
    Z -> 26
    AA -> 27
    AB -> 28 
    ...

---
+++ Excel Sheet Column Title.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int col) {
+        StringBuilder sb = new StringBuilder();
+        while(col > 0) {
+            int x = col % 26;
+            int rem = x == 0 ? 26 : x;
+            sb.append((char)(64 + rem));
+            col = (col - rem)/ 26 ;
+        }
+        return sb.reverse().toString();
+    }
+}
