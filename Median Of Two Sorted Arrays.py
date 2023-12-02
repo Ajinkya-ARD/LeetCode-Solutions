884ee856877c45b67f1174bdeda7d1f36d9a2059
Ajinkya Ravindra Deokate <129925084+Ajinkya-ARD@users.noreply.github.com>
2023-12-02 12:30:52 +0530

    LeetCode Solution #76
    4. Median of Two Sorted Arrays
    Hard
    
    Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
    
    The overall run time complexity should be O(log (m+n)).
    
     
    
    Example 1:
    
    Input: nums1 = [1,3], nums2 = [2]
    Output: 2.00000
    Explanation: merged array = [1,2,3] and median is 2.
    Example 2:
    
    Input: nums1 = [1,2], nums2 = [3,4]
    Output: 2.50000
    Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
     
    
    Constraints:
    
    nums1.length == m
    nums2.length == n
    0 <= m <= 1000
    0 <= n <= 1000
    1 <= m + n <= 2000
    -106 <= nums1[i], nums2[i] <= 106

---
+++ Median Of Two Sorted Arrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        num3 = sorted(nums1+nums2)
+        l=len(num3)
+        sum=0
+        li=[]
+        if l % 2 == 0:
+            return (num3[(l//2)-1]+num3[(l//2)])/2
+        else:
+            return num3[l//2]
