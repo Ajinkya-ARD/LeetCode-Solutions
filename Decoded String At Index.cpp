a148a08a9fd4280361648febb25f3781bcd575fd
Ajinkya Ravindra Deokate <129925084+Ajinkya-ARD@users.noreply.github.com>
2023-12-04 13:14:50 +0530

    LeetCode Solution #82
    880. Decoded String at Index
    Medium
    
    You are given an encoded string s. To decode the string to a tape, the encoded string is read one character at a time and the following steps are taken:
    
    If the character read is a letter, that letter is written onto the tape.
    If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
    Given an integer k, return the kth letter (1-indexed) in the decoded string.
    
     
    
    Example 1:
    
    Input: s = "leet2code3", k = 10
    Output: "o"
    Explanation: The decoded string is "leetleetcodeleetleetcodeleetleetcode".
    The 10th letter in the string is "o".
    Example 2:
    
    Input: s = "ha22", k = 5
    Output: "h"
    Explanation: The decoded string is "hahahaha".
    The 5th letter is "h".
    Example 3:
    
    Input: s = "a2345678999999999999999", k = 1
    Output: "a"
    Explanation: The decoded string is "a" repeated 8301530446056247680 times.
    The 1st letter is "a".
     
    
    Constraints:
    
    2 <= s.length <= 100
    s consists of lowercase English letters and digits 2 through 9.
    s starts with a letter.
    1 <= k <= 109
    It is guaranteed that k is less than or equal to the length of the decoded string.
    The decoded string is guaranteed to have less than 263 letters.

---
+++ Decoded String At Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long size = 0;  
+        for (auto ch : s)
+            if (isdigit(ch)) size *= ch - '0';
+            else ++size;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            k %= size;
+            if (k == 0 && isalpha(s[i])) return string(1, s[i]);
+            if (isdigit(s[i])) size /= s[i] - '0';
+            else --size;
+        }
+        return "";
+    }
+};
