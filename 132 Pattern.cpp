001f60a6e11b9a1774ee9c35ed5a41b0e7a1cc2f
Ajinkya Ravindra Deokate <129925084+Ajinkya-ARD@users.noreply.github.com>
2023-12-05 11:53:37 +0530

    LeetCode Solution #85
    456. 132 Pattern
    Medium
    
    Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
    
    Return true if there is a 132 pattern in nums, otherwise, return false.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,4]
    Output: false
    Explanation: There is no 132 pattern in the sequence.
    Example 2:
    
    Input: nums = [3,1,4,2]
    Output: true
    Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
    Example 3:
    
    Input: nums = [-1,3,2,0]
    Output: true
    Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
     
    
    Constraints:
    
    n == nums.length
    1 <= n <= 2 * 105
    -109 <= nums[i] <= 109

---
+++ 132 Pattern.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> min_array(n);
+        min_array[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            min_array[i] = min(min_array[i-1], nums[i]);
+        }
+        vector<int> st;
+        for(int i = n-1; i >= 0; i--) {
+            if(!st.empty() && nums[i] > min_array[i] && nums[i] < st.back() &&
+               min_array[i] < st.back()) st.push_back(nums[i]);
+            else  if(st.empty()) st.push_back(nums[i]);
+            else if(nums[i] == min_array[i]) continue;
+            else {
+                while(!st.empty() && nums[i] > st.back() && st.back() <= min_array[i]) st.pop_back();
+                if(!st.empty() && nums[i] > st.back() && st.back() > min_array[i]) return true;
+                else st.push_back(nums[i]);
+            }
+            
+        }
+        return false;
+    }
+};
